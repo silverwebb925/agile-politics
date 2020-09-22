ce024adab78e1d4b076730b6f231e9bd2d997b19
silverwebb925 <silverwebb925@users.noreply.github.com>
2020-09-22 14:33:02 -0700

    Gritty Hack 1.6

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,33 +9,9 @@ import { withPrefix } from 'gatsby'
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
   return (
-    <div>
-      <Helmet>
-        <html lang="en" />
-        <title>{title}</title>
-        <meta name="description" content="agile-politics" />
 
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
+      <div height="100vh">{children}</div>
 
-
-        <meta name="theme-color" content="#fff" />
-
-        <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
-        <meta
-          property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
-        />
-      </Helmet>
-
-      <div>{children}</div>
-
-    </div>
   )
 }
 
